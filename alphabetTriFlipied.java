d3dbd1286df738079b5b62bba91ced5395812ec5
Ashu Agrahari <118386268+AshuAgrahari21@users.noreply.github.com>
2024-08-15 21:40:33 +0530

    Add files via upload

---
+++ alphabetTriFlipied.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class alphabetTriFlipied {
+
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" "+" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
