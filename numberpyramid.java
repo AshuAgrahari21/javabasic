6723432707affe9b4759c07335aa1b624eb3e0bc
Ashu Agrahari <118386268+AshuAgrahari21@users.noreply.github.com>
2024-08-15 22:09:03 +0530

    Add files via upload

---
+++ numberpyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class numberpyramid {
+     public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+           
+            for(int j=1;j<=n-i;j++){
+               System.out.print(" "+" "); //for space 
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print(j+" "); // for tringle and add both 
+            }
+            System.out.println();
+        }
+
+    }
+}
