ba8f7f4b5dc26bc59ddc0b09db0cc237667c42cf
Ashu Agrahari <118386268+AshuAgrahari21@users.noreply.github.com>
2024-08-18 00:12:22 +0530

    Add files via upload

---
+++ maxInarray.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class maxInarray {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int arr[]={1000,20,30,45,432,3,123,456,21,24,564,900};
+        int max=-1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+               
+            }
+             
+        }
+        System.out.println("Maximum numer in array after 0 index "+max);
+       
+    }
+}
