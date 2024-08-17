a3eb031878adb57bc2d251d590903d6edd3265c4
Ashu Agrahari <118386268+AshuAgrahari21@users.noreply.github.com>
2024-08-18 00:16:00 +0530

    Update maxInarray.java

--- maxInarray.java
+++ maxInarray.java
@@ -5,7 +5,7 @@ public class maxInarray {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int arr[]={1000,20,30,45,432,3,123,456,21,24,564,900};
-        int max=-1;
+        int max=Integer.MIN_VALUE; //min value for int
         for(int i=1;i<arr.length;i++){
             if(arr[i]>max){
                 max=arr[i];
